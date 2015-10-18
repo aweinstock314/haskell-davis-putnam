4f9e4ceaf059d1324177400b868a50524154381e
Avi Weinstock <aweinstock314@gmail.com>
2015-10-18 10:53:50 -0400

    Implemented a simple version of the Davis-Putnam algorithm.

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,27 @@
-{-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction, StandaloneDeriving #-}
+{-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction #-}
 module Main where
 import Control.Monad
+import Data.Function
+import Data.Monoid
+import Data.Either
+import Data.List
 import Text.Parsec
 
+-- Data definitions
 data Identifier = Id String
+    deriving (Eq, Show)
 data LogicVar = Lit Identifier | Not Identifier
+    deriving (Eq, Show)
 
 data Formula = Var LogicVar | And Formula Formula | Or Formula Formula | Negate Formula
+    deriving (Eq, Show)
 
 data CNFClause = OrNode [LogicVar]
+    deriving (Eq, Show)
 data CNF = AndNode [CNFClause]
+    deriving (Eq, Show)
 
-deriving instance Show Identifier
-deriving instance Show LogicVar
-deriving instance Show Formula
-deriving instance Show CNFClause
-deriving instance Show CNF
-
+-- Parser definitions
 whitespace = many space
 
 parseNot = char '!' <|> char '~'
@@ -35,4 +40,47 @@ parseNegate = Negate <$> (parseNot *> whitespace *> parseFormula)
 parseCNFClause = OrNode <$> (parseLogicVar `sepBy1` (whitespace *> string "||" <* whitespace))
 parseCNF = AndNode <$> ((char '(' *> parseCNFClause <* char ')') `sepBy1` (whitespace *> string "&&" <* whitespace))
 
-main = forever (getLine >>= print . parse parseCNF "")
+-- Logic-y algorithms
+data BinTree a b = Leaf b | Node a (BinTree a b) (BinTree a b) deriving Show
+
+foldMapLeaves f = aux where
+    aux (Leaf b) = f b
+    aux (Node _ l r) = aux l <> aux r
+
+varName (Lit x) = x
+varName (Not x) = x
+
+reduceClauses :: LogicVar -> LogicVar -> CNF -> CNF
+reduceClauses x y = removeClausesContaining x . removeFromAllClauses y
+    where
+        removeClausesContaining v (AndNode clauses) = AndNode (filter (\(OrNode vars) -> not (v `elem` vars)) clauses)
+        removeFromAllClauses v (AndNode clauses) = AndNode (map (\(OrNode vars) -> OrNode (filter (/= v) vars)) clauses)
+
+davisPutnamTree :: CNF -> BinTree String (Bool, String)
+davisPutnamTree (AndNode clauses) =
+    let sortedClauses = AndNode (sortBy (compare `on` (\(OrNode vars) -> length vars)) clauses) in
+    -- TODO: {tautological elimination, subsumption elimination, pure literal elimination, unit rule} optimization passes
+    case sortedClauses of
+        AndNode [] -> Leaf (True, "no clauses left")
+        AndNode ((OrNode []):_) -> Leaf (False, "found an empty subclause")
+        AndNode ((OrNode (var:_)):_) -> let
+            i@(Id name) = varName var
+            tag = "branching on " <> name
+            leftResult = reduceClauses (Lit i) (Not i) sortedClauses
+            rightResult = reduceClauses (Not i) (Lit i) sortedClauses
+            in Node tag (davisPutnamTree leftResult) (davisPutnamTree rightResult)
+
+satisfiable :: CNF -> Bool
+satisfiable = getAny . foldMapLeaves (Any . fst) . davisPutnamTree
+
+-- Testing
+sampleFormulaeStrings = ["(~a || b) && (a)", "(a) && (a)", "(a) && (~a)", "(a || ~a)"]
+sampleFormulae = rights . map (parse parseCNF "") $ sampleFormulaeStrings
+
+--main = forever (getLine >>= print . parse parseCNF "")
+
+main = do
+    forM_ sampleFormulae $ \phi -> do
+        print phi
+        putStrLn $ if satisfiable phi then "Is satisfiable" else "Is not satisfiable"
+        putStrLn $ replicate 5 '-'
