0f7ef7f5ad376ada95b9b3f76f2e275bb585e59d
Avi Weinstock <aweinstock314@gmail.com>
2015-10-18 14:46:31 -0400

    Added diagrams of trees with arrows and colored nodes.

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE FlexibleContexts, NoMonomorphismRestriction #-}
 module Main where
 import Control.Monad
-import Data.Function
-import Data.Monoid
+import Control.Monad.State
 import Data.Either
+import Data.Function
 import Data.List
-import Text.Parsec
+import Data.Monoid
+import Text.Parsec hiding (State)
+import qualified Diagrams.Backend.SVG as D
+import qualified Diagrams.Prelude as D
 
 -- Data definitions
 data Identifier = Id String
@@ -63,7 +66,7 @@ reduceClauses x y = removeClausesContaining x . removeFromAllClauses y
         removeClausesContaining v (AndNode clauses) = AndNode (filter (\(OrNode vars) -> not (v `elem` vars)) clauses)
         removeFromAllClauses v (AndNode clauses) = AndNode (map (\(OrNode vars) -> OrNode (filter (/= v) vars)) clauses)
 
-davisPutnamTree :: CNF -> BinTree String (Bool, String)
+davisPutnamTree :: CNF -> BinTree (CNF, String) (Bool, String)
 davisPutnamTree (AndNode clauses) =
     let sortedClauses = AndNode (sortBy (compare `on` (\(OrNode vars) -> length vars)) clauses) in
     -- TODO: {tautological elimination, subsumption elimination, pure literal elimination, unit rule} optimization passes
@@ -75,20 +78,41 @@ davisPutnamTree (AndNode clauses) =
             tag = "branching on " <> name
             leftResult = reduceClauses (Lit i) (Not i) sortedClauses
             rightResult = reduceClauses (Not i) (Lit i) sortedClauses
-            in Node tag (davisPutnamTree leftResult) (davisPutnamTree rightResult)
+            in Node (sortedClauses, tag) (davisPutnamTree leftResult) (davisPutnamTree rightResult)
 
 satisfiable :: CNF -> Bool
 satisfiable = getAny . foldMapLeaves (Any . fst) . davisPutnamTree
 
+-- Diagram generation
+(#) = (D.#)
+
+treeDiagram :: BinTree (CNF, String) (Bool, String) -> D.Diagram D.B
+treeDiagram tree = evalState (aux tree) (0 :: Integer) where
+    aux (Leaf (sat, comment)) = return $ D.circle 1 # D.fc (if sat then D.green else D.red) # D.lw 0.5
+    aux (Node (cnf, comment) l r) = let
+        text s = D.text s <> (D.rect (genericLength s) 1 # D.lw D.none)
+        node = (D.vsep 1 [text (prettyCNF cnf ""), text comment])
+        freshName = modify (+1) >> get
+        (vl, vr) = (D.r2 (-1.0, -0.5), D.r2 (1.0,-0.5))
+        arrowOptions = D.def D.& D.headLength D..~ D.local 0.5 D.& D.shaftStyle D.%~ D.lw 1
+        in do
+            [lname, rname, nname] <- replicateM 3 freshName
+            l' <- D.named lname <$> aux l
+            r' <- D.named rname <$> aux r
+            let node' = D.named nname node
+            let connect = D.connect' arrowOptions nname
+            return $ (node' `D.appends` [(vl, l'), (vr, r')]) # connect lname # connect rname
+
 -- Testing
-sampleFormulaeStrings = ["(~a || b) && (a)", "(a) && (a)", "(a) && (~a)", "(a || ~a)"]
+sampleFormulaeStrings = ["(~a || b) && (a)", "(a) && (a)", "(a) && (~a)", "(a || ~a)", "(a || ~b || c) && (~d || e || ~f || g)"]
 sampleFormulae = rights . map (parse parseCNF "") $ sampleFormulaeStrings
 
 --main = forever (getLine >>= print . parse parseCNF "")
 
 main = do
-    forM_ sampleFormulae $ \phi -> do
+    forM_ (zip [0..] sampleFormulae) $ \(i, phi) -> do
         print phi
+        let s = 1000 in D.renderSVG ("formula" <> show i <> ".svg") (D.mkSizeSpec (D.r2 (Just s, Just s))) (treeDiagram (davisPutnamTree phi))
         putStrLn $ prettyCNF phi ""
         putStrLn $ if satisfiable phi then "Is satisfiable" else "Is not satisfiable"
         putStrLn $ replicate 5 '-'
